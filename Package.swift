6e3a2fc5cb64d2e26928455f5c934ebc33447a0e
Ryan VanAlstine <rtvanals@gmail.com>
2018-02-07 00:14:18 -0600

    Remove full vapor dependency

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 let package = Package(
     name: "FirebaseVerifier",
     dependencies: [
-        .Package(url: "https://github.com/vapor/jwt.git",
-                 majorVersion: 2),
-        .Package(url: "https://github.com/vapor/vapor.git",
-                 majorVersion: 2),
+        .Package(url: "https://github.com/vapor/jwt.git", majorVersion: 2)
         ]
 )
